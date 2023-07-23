265dc81030b9778e1eeed480b7aeea1704942fa3
Yanis-HAOUCHET <haouchet.yh@etu.univ-blida.dz>
2023-07-24 01:46:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './Components/pages/Home';
+
+function App() {
+  return (
+    <div className='app'>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
